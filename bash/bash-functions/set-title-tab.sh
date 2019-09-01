3774abb182e90fc75ca0fcff7e1074b23410c7b7
napicella <napicella4@gmail.com>
2019-09-01 17:43:25 +0300

    Create set-title-tab.sh

---
+++ bash/bash-functions/set-title-tab.sh
@@ -0,0 +1,10 @@
+# function to set terminal title
+# Add this to your .bashrc or 
+# bash profile
+function set-title() {
+  if [[ -z "$ORIG" ]]; then
+    ORIG=$PS1
+  fi
+  TITLE="\[\e]2;$*\a\]"
+  PS1=${ORIG}${TITLE}
+}
